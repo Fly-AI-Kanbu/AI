c521e3ff7f4f9ba1372cfa1043744ef74405334e
jungch15 <58167392+jungch15@users.noreply.github.com>
2024-08-22 19:10:49 +0900

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -13,3 +13,20 @@ class TextInput(BaseModel):
 
 class TextInputs(BaseModel):
     inputs: List[TextInput]
+
+
+class modelInputBase(BaseModel):
+    model1: str
+    user: str
+    model2: str
+
+class modelInputsBase(BaseModel):
+    inputs: List[modelInputBase]
+
+class modelInput(modelInputBase):
+    class Config:
+        from_attributes = True
+
+class modelInputs(modelInputsBase):
+    class Config:
+        from_attributes = True
