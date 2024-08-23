130285f060b74b3652f114fa9d2694e6285e5343
ljh20011 <ljh20011@naver.com>
2024-08-23 17:08:52 +0900

    change model

--- aimodels.py
+++ aimodels.py
@@ -1,36 +1,27 @@
 import torch
 import torch.nn as nn
 from sklearn.metrics.pairwise import cosine_similarity
-from transformers import BertTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import warnings
 
 warnings.filterwarnings("ignore", message="A parameter name that contains `gamma` will be renamed internally to `weight`.")
 warnings.filterwarnings("ignore", message="A parameter name that contains `beta` will be renamed internally to `bias`.")
 
-# BERT 모델 정의 (분류 레이어 추가)
-class BertClassifier(nn.Module):
-    def __init__(self, bert_model, num_labels):
-        super(BertClassifier, self).__init__()
-        self.bert = bert_model
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
-    
-    def forward(self, input_ids, attention_mask, token_type_ids=None):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        cls_output = outputs.last_hidden_state[:, 0, :]  # [CLS] 토큰의 임베딩
-        logits = self.classifier(cls_output)
-        return logits
+
 
 # 토크나이저 및 BERT 모델 로드
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-bert_model = BertModel.from_pretrained('bert-base-uncased')
+model_name = 'monologg/koelectra-base-v3-discriminator'
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
+checkpoint_path = "weight/checkpoint.bin"
 
-# 분류 레이어를 가진 새로운 모델 생성
-model = BertClassifier(bert_model, num_labels=2)  # 예제에서는 2개의 클래스를 분류한다고 가정
+model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cpu')))
+model.eval() 
 
 # CUDA 사용 가능 여부 확인 (가능하면 GPU 사용)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def process_input(user_input):
     # 토큰화 및 인코딩
